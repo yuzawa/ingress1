78435681d1bea3598d4534c1db9fe75655b03520
Manabu Yuzawa <yuzawa@waltz.plala.or.jp>
2015-04-02 18:52:20 +0900

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ingress1::Application.config.secret_key_base = 'cdf9fd0874e9e55e63f54fc7ba4eb71d9b78c22dce2ea57e30ffa51d3f9c7d51cb107d3259c007996a1fbd8518601da154530635dad8b6439229f6048a0e9bc4'
